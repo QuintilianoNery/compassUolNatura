df073c6daa4fb664aca3d7031089d0521ad566f8
Quintiliano <quintiliano1995@gmail.com>
2023-09-07 18:03:42 -0300

    Configuração Biblioteca

--- cypress.config.js
+++ cypress.config.js
@@ -11,9 +11,13 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // allureWriter(on, config)
+      require('@cypress/grep/src/plugin')(config);
+      return config;
     },
     baseUrl: 'https://www.natura.com.br/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+
+
   },
   experimentalStudio: true
 });
